10ce77544d25d061acb7efe6737a9a261024e0dc
ayalag <ayala.gottfried@xmcyber.com>
2025-02-03 11:32:07 +0200

    costs calculator interface

---
+++ apps/operator/src/costs-calculator/costs-calculator.ts
@@ -0,0 +1,10 @@
+class CostsCalculator {
+    calculateNodeCost(instanceType: string, capacity: string, type: string) : number {
+    }
+
+    calculatePvcCost(capacity: string, storageType: string, storageClass) : number {
+    }
+
+    calculateLoadBalancercCost(type: string) : number {
+    }
+}
